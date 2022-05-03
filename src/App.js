4d4c4b0c6355a4987cba83bfd0dd76a808a802ea
Lipon Roy <liponbdbd@gmail.com>
2022-05-03 23:24:23 +0600

    castom Navbar using javascript and basic routing has done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './component/Home/Home.js';
+import Contact from './component/Contact/Contact.js';
+import About from './component/About/About.js';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './component/NotFound/NotFound';
+import NavbarTwo from './component/Navbar/NavbarTwo';
+import Profile from './component/Profile/Profile';
+import DeshBoard from './component/deshBoard/DeshBoard';
+import { createContext, useState } from 'react';
+// context Api use
+export const myContext = createContext();
 function App() {
+	const [product, setProduct] = useState([]);
 	return (
 		<div className='App'>
-			<h1>Project start</h1>
+			<myContext.Provider value={[product, setProduct]}>
+				<NavbarTwo></NavbarTwo>
+
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/profile' element={<Profile />} />
+
+					<Route path='/deshBoard/:id' element={<DeshBoard />} />
+					{/* <Route path='/blog/:id' element={<About />} /> */}
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</myContext.Provider>
 		</div>
 	);
 }
